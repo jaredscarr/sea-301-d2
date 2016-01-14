ff2e90f4b305270bfc95a87c1a47e568f81f2b9b
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-13 16:55:41 -0800

    Test

--- class-08-crud-a-resource/lecture-code/miniDB/model.js
+++ class-08-crud-a-resource/lecture-code/miniDB/model.js
@@ -4,3 +4,6 @@ model.setupTable = function() {
   webDB.runSQLcmd('DROP TABLE articles');
   webDB.runSQLcmd('CREATE TABLE IF NOT EXISTS articles (title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, body TEXT NOT NULL);');
 };
+
+
+//the above is the columns entered into string form
