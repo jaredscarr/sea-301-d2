3fd03df346add4d0a12eaa964ee5632d523d13a5
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-01-18 13:31:43 -0800

    completed all todos MVP reached. Assignment complete.

---
+++ class-11-spa-routing/pair-assignment/Andy-jared/starter-code/scripts/articleController.js
@@ -0,0 +1,17 @@
+(function(module) {
+  var articlesController = {};
+
+  // DONE: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
+  Article.createTable();
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // code that used to be in index.html.
+  Article.fetchAll(articleView.initIndexPage);
+  // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articlesController.index = function() {
+    $('.tab-content').hide();
+    console.log('running function');
+    $('#articles').fadeIn();
+  };
+
+  module.articlesController = articlesController;
+})(window);
---
+++ class-11-spa-routing/pair-assignment/Andy-jared/starter-code/scripts/routes.js
@@ -0,0 +1,18 @@
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
+// linked to a a single controller function to handle it:
+page.base('');
+
+page('/index', index);
+page('/about', about);
+// page('*', notFound);
+
+// DONE: What function do you call to activate page.js? Fire it off now, to execute
+page();
+
+function index() {
+  articlesController.index();
+}
+
+function about() {
+  aboutController.index();
+}
